10e12a8ffbfceb0466dfc4fe1cbf06a9714a5b39
amelendez <abisaimelendez@icloud.com>
2024-10-23 09:25:11 -0600

    commit

--- build.sh
+++ build.sh
@@ -3,5 +3,14 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-#python manage.py collectstatic --no-input
+# Ejecuta las migraciones
 python manage.py migrate
+
+# Crea el superusuario automáticamente
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username='amelendez').exists():
+    User.objects.create_superuser('amelendez', 'amelendez@example.com', 'Jodente_03')
+END
