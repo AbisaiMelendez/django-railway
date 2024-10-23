2e0dbf391d75ba67bf54847f9fec1ecb7005ac39
amelendez <abisaimelendez@icloud.com>
2024-10-23 09:26:08 -0600

    finish

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 if not User.objects.filter(username='amelendez').exists():
-    User.objects.create_superuser('amelendez', 'amelendez@example.com', 'Jodente_03')
+    User.objects.create_superuser('amelendez', 'amelendez@surgepays.com', 'Jodente_03')
 END
