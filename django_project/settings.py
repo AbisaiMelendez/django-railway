dceac783b4b1511dd9e4a1c232a11e0963e10647
amelendez <abisaimelendez@icloud.com>
2024-10-23 08:54:43 -0600

    ready for render.com

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +22,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0yddivx#&$#-m$=6gp*e8q9*fxxn57_%3j$ew7=^to2xc#j_#@'
+# SECRET_KEY = 'django-insecure-0yddivx#&$#-m$=6gp*e8q9*fxxn57_%3j$ew7=^to2xc#j_#@'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
+
 
 ALLOWED_HOSTS = []
 
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 # Application definition
 
 INSTALLED_APPS = [
@@ -78,10 +85,16 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        #default='postgresql://administrator:erCiG4ExgyUq45MIZAHWaCQHZzGGxvmy@dpg-cscg48jtq21c7396hn40-a/surgepays_it',
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
