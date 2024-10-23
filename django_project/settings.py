214748da0a894e306bea3f0c3c453d7e846b6eb2
amelendez <abisaimelendez@icloud.com>
2024-10-23 14:44:33 -0600

    update finish

--- django_project/settings.py
+++ django_project/settings.py
@@ -148,3 +148,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #         'rest_framework.permissions.IsAuthenticated',
 #     ],
 # }
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
--- projects/api.py
+++ projects/api.py
@@ -1,31 +1,34 @@
 from rest_framework import viewsets
 from .models import Brand, Model, Series, Category, Inventory
 from .serializers import BrandSerializer, ModelSerializer, SeriesSerializer, CategorySerializer, InventorySerializer
-from rest_framework import permissions
+#from rest_framework import permissions
+from rest_framework.permissions import AllowAny
 
 
 class BrandViewSet(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+    permission_classes = [AllowAny]
 
 class ModelViewSet(viewsets.ModelViewSet):
     queryset = Model.objects.all()
     serializer_class = ModelSerializer
-
+    permission_classes = [AllowAny]
 class SeriesViewSet(viewsets.ModelViewSet):
     queryset = Series.objects.all()
     serializer_class = SeriesSerializer
-
+    permission_classes = [AllowAny]
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
+class InventoryViewSet(viewsets.ModelViewSet):
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
+    permission_classes = [AllowAny]
 
 class InventoryViewSet(viewsets.ModelViewSet):
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
-
-
-class InventoryViewSet(viewsets.ModelViewSet):
-    queryset = Inventory.objects.all()
-    serializer_class = InventorySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Solo autenticados pueden modificar
+    permission_classes = [AllowAny]  # Solo autenticados pueden modificar
+    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Solo autenticados pueden modificar
