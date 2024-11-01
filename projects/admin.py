7f5fc93c78f0c979f5cbedba6ca52edecfae458c
amelendez <abisaimelendez@icloud.com>
2024-11-01 13:00:02 -0600

    set

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Brand
+from .models import Model
+from .models import Series
+from .models import Category
+from .models import Inventory
+
+admin.site.register(Brand)
+admin.site.register(Model)
+admin.site.register(Series)
+admin.site.register(Category)
+admin.site.register(Inventory)
